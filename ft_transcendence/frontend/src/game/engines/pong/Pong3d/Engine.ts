90def50c1aed86bb6f21371ddea000544de4f9d3
joaorema <joaopcrema@gmail.com>
2025-10-21 17:44:33 +0100

    Added: Goal Image on backwall changed the code (parsed it to smaller fts to keep contructor cleaner) will now  try to move the ball

--- ft_transcendence/frontend/src/game/engines/pong/Pong3d/Engine.ts
+++ ft_transcendence/frontend/src/game/engines/pong/Pong3d/Engine.ts
@@ -1,132 +1,136 @@
-import * as BABYLON from '@babylonjs/core';
+import * as BABYLON from "@babylonjs/core";
 import { Skybox } from "./Skybox";
 
-
-export class Pong3Dscene
+export class Pong3Dscene 
 {
-    private engine: BABYLON.Engine;         //engine
+    private engine: BABYLON.Engine;
     private scene: BABYLON.Scene;
-    private camera: BABYLON.FreeCamera;
-    private light: BABYLON.HemisphericLight;
-    private ball: BABYLON.Mesh;
-    private paddle_left: BABYLON.Mesh;
-    private paddle_right: BABYLON.Mesh
+    private camera!: BABYLON.FreeCamera;
+    private light!: BABYLON.HemisphericLight;
+    private ball!: BABYLON.Mesh;
+    private paddle_left!: BABYLON.Mesh;
+    private paddle_right!: BABYLON.Mesh;
 
-    constructor(private canvas: HTMLCanvasElement)
-    {
-        //start engine
+    private keys: Record<string, boolean> = {};
+
+    constructor(private canvas: HTMLCanvasElement) {
         this.engine = new BABYLON.Engine(canvas, true);
-        
-        //avoid error
+        this.scene = new BABYLON.Scene(this.engine);
+        this.scene.clearColor = new BABYLON.Color4(0, 0, 0, 1);
+
+        this.setupInput();
+        this.createCamera();
+        this.createLight();
+        this.createEnvironment();
+        this.createGameObjects();
+        this.enableCollisions();
+        this.setupGameLoop();
         console.log(this.canvas);
-        //create scene
-        this.scene = new  BABYLON.Scene(this.engine);
-        this.scene.clearColor = new BABYLON.Color4(0,0,0,1);
 
-        //check keys
-        const keys: Record<string, boolean> = {};
-        this.scene.onKeyboardObservable.add((kbInfo) =>{
-            switch(kbInfo.type){
-                case BABYLON.KeyboardEventTypes.KEYDOWN:
-                    keys[kbInfo.event.key.toLowerCase()] = true;
-                    break;
-                case BABYLON.KeyboardEventTypes.KEYUP:
-                    keys[kbInfo.event.key.toLowerCase()] = false;
-                    break;
-            }
-        })
-        //setup camera
-        this.camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(-10,2,0), this.scene);
-        
-        //setup lights
-        this.light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0,1,0), this.scene);
+        window.addEventListener("resize", () => this.engine.resize());
+    }
+
+    private setupInput() 
+    {
+        this.scene.onKeyboardObservable.add((kbInfo) => {
+            const key = kbInfo.event.key.toLowerCase();
+            this.keys[key] = kbInfo.type === BABYLON.KeyboardEventTypes.KEYDOWN;
+        });
+    }
+
+    private createCamera() 
+    {
+        this.camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(-10, 2, 0), this.scene);
+    }
+
+    private createLight() 
+    {
+        this.light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), this.scene);
         this.light.intensity = 0.8;
-        
-        
-        //ground setup(floor)
-        const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 20, height: 20}, this.scene);
-        
-        //wall setup
-        const leftWall = BABYLON.MeshBuilder.CreateBox("left_wall", {width: 0.5, height:3, depth:20}, this.scene);
-        leftWall.position = new BABYLON.Vector3(0,1,10);
-        leftWall.rotation.y = Math.PI / 2;
-        const rightWall = leftWall.clone("right_wall");
-        rightWall.position = new BABYLON.Vector3(0,1,-10);
-        
-        this.camera.setTarget(BABYLON.Vector3.Zero());
-        
-        //ball setup
-        this.ball = BABYLON.MeshBuilder.CreateSphere("ball", {diameter: 0.5}, this.scene);
-        this.ball.position = new BABYLON.Vector3(0, 1, 0);
+    }
 
-        //Padles setup
-        this.paddle_left = BABYLON.MeshBuilder.CreateBox("left_paddle", {width: 0.4, height: 1, depth: 2}, this.scene);
+    private createEnvironment() 
+    {
+        // Ground
+        const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 20, height: 20 }, this.scene);
+        const groundMat = new BABYLON.StandardMaterial("groundMat", this.scene);
+        groundMat.diffuseColor = new BABYLON.Color3(0.26, 0.89, 0.07);
+        ground.material = groundMat;
+
+        // Walls
+        const wallMat = new BABYLON.StandardMaterial("wallMat", this.scene);
+        wallMat.diffuseColor = new BABYLON.Color3(0, 0.5, 0.5);
+
+        const leftWall = BABYLON.MeshBuilder.CreateBox("left_wall", { width: 0.5, height: 3, depth: 20 }, this.scene);
+        leftWall.position = new BABYLON.Vector3(0, 1.5, 10);
+        leftWall.rotation.y = Math.PI / 2;
+        leftWall.material = wallMat;
+
+        const rightWall = leftWall.clone("right_wall");
+        rightWall.position = new BABYLON.Vector3(0, 1.5, -10);
+        rightWall.material = wallMat;
+
+        // Goal
+        const goalPlane = BABYLON.MeshBuilder.CreatePlane("goalPlane", { width: 20, height: 3 }, this.scene);
+        goalPlane.position = new BABYLON.Vector3(10, 1.5, 0);
+        goalPlane.rotation = new BABYLON.Vector3(0, Math.PI / 2, 0);
+
+        const goalMat = new BABYLON.StandardMaterial("goalMat", this.scene);
+        goalMat.diffuseTexture = new BABYLON.Texture("assets/images/goaltest.png", this.scene);
+        goalMat.diffuseTexture.hasAlpha = true;
+        goalMat.backFaceCulling = false;
+        goalPlane.material = goalMat;
+
+        Skybox.create(this.scene, "assets/images/skyboxpong.hdr");
+    }
+
+    private createGameObjects() 
+    {
+        // Ball
+        this.ball = BABYLON.MeshBuilder.CreateSphere("ball", { diameter: 0.5 }, this.scene);
+        const ballMat = new BABYLON.StandardMaterial("ballMat", this.scene);
+        ballMat.diffuseColor = new BABYLON.Color3(1, 1, 1);
+        this.ball.material = ballMat;
+        this.ball.position = new BABYLON.Vector3(0, 0.5, 0);
+
+        // Paddles
+        const paddleMat = new BABYLON.StandardMaterial("paddleMat", this.scene);
+        paddleMat.diffuseColor = new BABYLON.Color3(0.9, 0.1, 0.1);
+
+        this.paddle_left = BABYLON.MeshBuilder.CreateBox("left_paddle", { width: 0.4, height: 1, depth: 2 }, this.scene);
         this.paddle_left.position = new BABYLON.Vector3(-8, 0.5, 0);
-        
-        this.paddle_right = BABYLON.MeshBuilder.CreateBox("right_paddle", {width: 0.4, height: 1, depth: 2}, this.scene);
+        this.paddle_left.material = paddleMat;
+
+        this.paddle_right = BABYLON.MeshBuilder.CreateBox("right_paddle", { width: 0.4, height: 1, depth: 2 }, this.scene);
         this.paddle_right.position = new BABYLON.Vector3(8, 0.5, 0);
-        
-        //set camera to follow paddle
+        this.paddle_right.material = paddleMat;
+
         this.camera.parent = this.paddle_left;
         this.camera.position = new BABYLON.Vector3(-12, 2, 0);
-        this.camera.setTarget(this.paddle_left.position.add(new BABYLON.Vector3(10,0,0)));
+        this.camera.setTarget(this.paddle_left.position.add(new BABYLON.Vector3(10, 0, 0)));
+    }
 
-        //Materials
-        const mat = new BABYLON.StandardMaterial("mat", this.scene);
-        mat.diffuseColor = new BABYLON.Color3(0,0.5,0.5);
-        this.paddle_left.material = mat;
-        this.paddle_right.material = mat;
-        leftWall.material = mat;
-        rightWall.material = mat;
-        
-        //ground color
-        const greenmat = new BABYLON.StandardMaterial("greenmat", this.scene);
-        greenmat.diffuseColor = new BABYLON.Color3(0.26, 0.89, 0.07);
-        ground.material = greenmat;
-        
-        //ball color
-        const ballColor = new BABYLON.StandardMaterial("ballcolor", this.scene);
-        ballColor.diffuseColor = new BABYLON.Color3(1,1,1);
-        this.ball.material = ballColor;
-
-        //skybox
-        Skybox.create(this.scene, "assets/images/skytest.hdr")
-        
-        //collision
-        this.paddle_left.checkCollisions = true;
-        this.paddle_left.ellipsoid = new BABYLON.Vector3(0.2, 0.5, 1); // half-size of your paddle
-        this.paddle_left.ellipsoidOffset = new BABYLON.Vector3(0, 0.5, 0); // center it vertically
-
-        leftWall.checkCollisions = true;
-        rightWall.checkCollisions = true;
+    private enableCollisions() 
+    {
         this.scene.collisionsEnabled = true;
-        ground.checkCollisions = true;
-      
-        
 
-        // Movement with collisions
+        this.paddle_left.checkCollisions = true;
+        this.paddle_right.checkCollisions = true;
+    }
+
+    private setupGameLoop() 
+    {
         this.scene.onBeforeRenderObservable.add(() => {
             const moveVector = new BABYLON.Vector3(0, 0, 0);
-
-            if (keys["a"]) moveVector.z += 0.1;  // move left
-            if (keys["d"]) moveVector.z -= 0.1;  // move right
-
+            if (this.keys["a"]) moveVector.z += 0.1;
+            if (this.keys["d"]) moveVector.z -= 0.1;
             this.paddle_left.moveWithCollisions(moveVector);
         });
 
-        //simple start up to test
-        this.engine.runRenderLoop(() =>{
-            this.scene.render();
-        })
-
-        //resize
-        window.addEventListener("resize", ()=>{
-            this.engine.resize();
-        });
-
+        this.engine.runRenderLoop(() => this.scene.render());
     }
 
-    dispose(): void
+    dispose() 
     {
         this.engine.stopRenderLoop();
         this.scene.dispose();
