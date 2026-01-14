6f43b4e135c9a7b8707bf3bfda626960493f7057
Team <joaopcrema@gmail.com>
2026-01-14 16:49:19 +0000

    Possible fix for prometheus service. new docker file and changes to entrypoint and docker compose

--- ft_transcendence/infrastructure/monitoring/prometheus/entrypoint.sh
+++ ft_transcendence/infrastructure/monitoring/prometheus/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+# Read secrets (This will work if the VM permissions allow it)
 METRICS_USER="$(cat /run/secrets/metrics_user)"
 METRICS_PASS="$(cat /run/secrets/metrics_pass)"
 
@@ -9,11 +10,14 @@ esc() { printf '%s' "$1" | sed -e 's/[\/&]/\\&/g'; }
 MU="$(esc "$METRICS_USER")"
 MP="$(esc "$METRICS_PASS")"
 
+# Output to our 'gen' sub-directory
 sed \
   -e "s|\${METRICS_USER}|$MU|g" \
   -e "s|\${METRICS_PASS}|$MP|g" \
-  /etc/prometheus/prometheus.yml.tpl > /etc/prometheus/prometheus.yml
+  /etc/prometheus/prometheus.yml.tpl > /etc/prometheus/gen/prometheus.yml
 
+# Start Prometheus pointing to the generated file
 exec /bin/prometheus \
-  --config.file=/etc/prometheus/prometheus.yml \
-  --storage.tsdb.path=/prometheus
+  --config.file=/etc/prometheus/gen/prometheus.yml \
+  --storage.tsdb.path=/prometheus \
+  --web.enable-lifecycle
